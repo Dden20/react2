8623da149c983162e9baf975f7ea96b720fd51cd
Denis <ebay20200202@gmail.com>
2020-04-12 17:34:01 +0200

    Updates

---
+++ precache-manifest.70d704d1256b017ebc356abd2bb27d9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cc972dab86e0fdc6ad1210d1362661",
+    "url": "/react2/index.html"
+  },
+  {
+    "revision": "bd3a3525c21b35b79d2d",
+    "url": "/react2/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b960e63144ff738459b0",
+    "url": "/react2/static/css/main.73dd182d.chunk.css"
+  },
+  {
+    "revision": "bd3a3525c21b35b79d2d",
+    "url": "/react2/static/js/2.41c7657c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react2/static/js/2.41c7657c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b960e63144ff738459b0",
+    "url": "/react2/static/js/main.011ba54f.chunk.js"
+  },
+  {
+    "revision": "de1e06ef149f5cb620ba",
+    "url": "/react2/static/js/runtime-main.7f09f70d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react2/static/media/SEGA.12a3db8b.woff"
+  }
+]);
